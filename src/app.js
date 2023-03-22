5773d6d0cb0816282c4c12a3a6498d90500310d5
Nguyen Khac Vu <vunkph19573@fpt.edu.vn>
2023-03-22 13:35:47 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import productRouter from './routers/product';
-const app = express();
+import productRouter from "./routers/product";
 
-app.use = (express.json());
-app.use = ("/api", productRouter);
-mongoose.connect("mongodb://localhost:27017/we17301")
+const app = express();
+app.use(express.json());
+app.use("/api", productRouter);
+mongoose.connect("mongodb://localhost:27017/we17301");
 export const viteNodeApp = app;
--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,4 +1,4 @@
-import Product from "../model/product"
+import Product from "../models/product"
 import Joi from "joi";
 
 const productSchema = Joi.object(
@@ -33,16 +33,32 @@ export const get = async (req, res) => {
 }
 export const create = async (req, res) => {
     try {
+        const { error } = productSchema.validate(req.body);
+        if (error) {
+            const message = error.details[0].message;
+            return res.status(400).json({ message })
+        }
         const product = await Product.create(req.body);
-        return product;
+        return res.json({
+            message: "them thanh cong",
+            product,
+        });
     } catch (error) {
         res.status(404).json({ message: error });
     }
 }
 export const update = async (req, res) => {
     try {
+        const { error } = productSchema.validate(req.body);
+        if (error) {
+            const message = error.details[0].message;
+            return res.status(400).json({ message })
+        }
         const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        return response.status(200).json(product);
+        return response.status(200).json({
+            message: "sua thanh cong",
+            product
+        });
     } catch (error) {
         res.status(400).json({ message: error });
     }
@@ -50,7 +66,10 @@ export const update = async (req, res) => {
 export const remove = async (req, res) => {
     try {
         const product = await Product.findByIdAndDelete(req.params.id);
-        return res.status(200).json(product);
+        return res.status(200).json({
+            message: 'xoa thanh cong',
+            product,
+        });
     } catch (error) {
         res.status(200).json({ message: error });
     }
